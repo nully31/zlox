44f673cfec8f4e7271089b03ef15d559f822d3f0
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-15 00:22:13 +0900

    Implement grow func

--- src/chunk.zig
+++ src/chunk.zig
@@ -9,20 +9,22 @@ pub const Opcode = enum {
 
 /// A dynamic array structure of instructions.
 pub const Chunk = struct {
-    count: u32 = 0,
-    capacity: u32 = 0,
-    code: ?*u8 = null,
+    count: usize = 0,
+    capacity: usize = 0,
+    code: []u8 = &.{},
     allocator: Allocator = gpa.allocator(),
 
     pub fn write(self: *Chunk, byte: u8) void {
         // if the current chunk doesn't have enough capacity, then grow itself by doubling the capacity.
-        if (self.capacity < self.count + 1) {
-            const old_capacity = self.capacity;
-            self.capacity = if (self.capacity < 8) 8 else self.capacity * 2;
+        if (self.code.len < self.count + 1) {
+            self.capacity = if (self.code.len < 8) 8 else self.code.len * 2;
             self.code = self.allocator.realloc(self.code, self.capacity) catch {
-                self.allocator.destroy(self.code);
+                self.allocator.free(self.code);
                 std.os.exit(1);
             };
         }
+
+        self.code[self.count] = byte;
+        self.count += 1;
     }
 };
