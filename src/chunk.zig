dd579418fc8dbcf1019af30117abdfea4a52262a
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-14 20:01:37 +0900

    Add `realloc()` exception handler

--- src/chunk.zig
+++ src/chunk.zig
@@ -17,7 +17,10 @@ pub const Chunk = struct {
         if (self.capacity < self.count + 1) {
             const old_capacity = self.capacity;
             self.capacity = if (self.capacity < 8) 8 else self.capacity * 2;
-            self.code = try self.allocator.realloc(self.code, self.capacity);
+            self.code = self.allocator.realloc(self.code, self.capacity) catch {
+                self.allocator.destroy(self.code);
+                std.os.exit(1);
+            };
         }
     }
 };
