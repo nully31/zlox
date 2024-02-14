81facaba7168bc15260ba6b3e98fbd27547a37ec
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-14 17:40:54 +0900

    Define `Chunk` struct

--- src/chunk.zig
+++ src/chunk.zig
@@ -1,5 +1,23 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 pub const Opcode = enum {
     OP_RETURN,
+};
+
+pub const Chunk = struct {
+    count: u32 = 0,
+    capacity: u32 = 0,
+    code: ?*u8 = null,
+    allocator: Allocator = gpa.allocator(),
+
+    pub fn write(self: *Chunk, byte: u8) void {
+        // if the current chunk doesn't have enough capacity, then grow itself by doubling the capacity.
+        if (self.capacity < self.count + 1) {
+            const old_capacity = self.capacity;
+            self.capacity = if (self.capacity < 8) 8 else self.capacity * 2;
+            self.code = try self.allocator.realloc(self.code, self.capacity);
+        }
+    }
 };
