1bb7d846032d69714f4bb91f5c309814eacc1b34
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-18 16:44:26 +0900

    Tokenize symbols, comments and literals (and scan whitespace chars)

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -25,17 +25,70 @@ pub fn init(source: []const u8) Scanner {
     };
 }
 
+fn isDigit(char: u8) bool {
+    return char >= '0' and char <= '9';
+}
+
 /// Scan and tokenize the given source input.
 /// This is the core method of the scanner struct.
 pub fn scanToken(self: *Scanner) Token {
+    self.skipWhitespace();
     self.start = self.current;
     if (self.isAtEnd()) return self.makeToken(TokenType.EOF);
 
-    return self.errorToken("Unexpected character.");
+    const c = self.advance();
+    if (isDigit(c)) return number();
+
+    switch (c) {
+        '(' => return self.makeToken(TokenType.LEFT_PAREN),
+        ')' => return self.makeToken(TokenType.RIGHT_PAREN),
+        '{' => return self.makeToken(TokenType.LEFT_BRACE),
+        '}' => return self.makeToken(TokenType.RIGHT_BRACE),
+        ';' => return self.makeToken(TokenType.SEMICOLON),
+        ',' => return self.makeToken(TokenType.COMMA),
+        '.' => return self.makeToken(TokenType.DOT),
+        '-' => return self.makeToken(TokenType.MINUS),
+        '+' => return self.makeToken(TokenType.PLUS),
+        '/' => return self.makeToken(TokenType.SLASH),
+        '*' => return self.makeToken(TokenType.STAR),
+        '!' => return self.makeToken(if (self.match('=')) TokenType.BANG_EQUAL else TokenType.BANG),
+        '=' => return self.makeToken(if (self.match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL),
+        '<' => return self.makeToken(if (self.match('=')) TokenType.LESS_EQUAL else TokenType.LESS),
+        '>' => return self.makeToken(if (self.match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER),
+        '"' => return self.string(),
+        else => return self.errorToken("Unexpected character."),
+    }
 }
 
 fn isAtEnd(self: *Scanner) bool {
     return self.current == self.source.len;
+}
+
+/// Consumes the current character.
+fn advance(self: *Scanner) u8 {
+    self.current += 1;
+    return self.source[self.current - 1];
+}
+
+/// Returns the current character without consuming it (lookahead).
+fn peek(self: *Scanner) u8 {
+    return self.source[self.current];
+}
+
+/// Looks ahead of the current character and returns the character
+/// without consuming neither it nor the current character.
+/// Returns `null` if the current character is the last character (so the next one doesn't exist).
+fn peekNext(self: *Scanner) ?u8 {
+    if (self.isAtEnd()) return null;
+    return self.source[self.current + 1];
+}
+
+/// Consumes the current character if and only if it matches the expected.
+fn match(self: *Scanner, expected: u8) bool {
+    if (self.isAtEnd()) return false;
+    if (self.source[self.current] != expected) return false; // Lookahead
+    self.current += 1;
+    return true;
 }
 
 fn makeToken(self: *Scanner, Type: TokenType) Token {
@@ -48,6 +101,61 @@ fn errorToken(self: *Scanner, message: []const u8) Token {
         .lexeme = message,
         .line = self.line,
     };
+}
+
+/// Simply consumes whitespace characters.
+fn skipWhitespace(self: *Scanner) void {
+    while (true) {
+        switch (self.peek()) {
+            ' ' => self.advance(),
+            '\r' => self.advance(),
+            '\t' => self.advance(),
+            '\n' => {
+                self.line += 1;
+                self.advance();
+            },
+            '/' => {
+                if (self.peekNext()) |c| {
+                    if (c == '/') {
+                        // A comment goes until the end of the line
+                        while (self.peek() != '\n' and !self.isAtEnd()) self.advance();
+                    } else {
+                        return;
+                    }
+                } else {
+                    return;
+                }
+            },
+            else => return,
+        }
+    }
+}
+
+fn number(self: *Scanner) Token {
+    while (isDigit(self.peek())) self.advance();
+
+    // Look for a fractional part
+    if (self.peek() == '.' and isDigit(self.peekNext())) {
+        // Consume the "."
+        self.advance();
+
+        while (isDigit(self.peek())) self.advance();
+    }
+
+    return self.makeToken(TokenType.NUMBER);
+}
+
+fn string(self: *Scanner) Token {
+    while (self.peek() != '"' and !self.isAtEnd()) {
+        if (self.peek() == '\n') self.line += 1;
+        self.advance();
+    }
+
+    if (self.isAtEnd()) return self.errorToken("Unterminated string.");
+
+    // The closing quote
+    self.advance();
+    return self.makeToken(TokenType.STRING);
 }
 
 /// Types of tokens.
