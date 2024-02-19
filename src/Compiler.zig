ed839ac02e03797f72e0facdefe872332d9be1a9
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-19 22:11:37 +0900

    Separate `Parser` to another file

--- src/Compiler.zig
+++ src/Compiler.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Chunk = @import("Chunk.zig");
 const Scanner = @import("Scanner.zig");
+const Parser = @import("Parser.zig");
 const Token = Scanner.Token;
 const TokenType = Scanner.TokenType;
 const InterpretError = @import("VM.zig").InterpretError;
@@ -77,50 +78,3 @@ fn end(self: *Compiler) !void {
 fn currentChunk(self: *Compiler) *Chunk {
     return self.compiling_chunk;
 }
-
-/// Parser struct. It is part of the Compiler struct.
-const Parser = struct {
-    current: Token,
-    previous: Token,
-    hadError: bool,
-    panicMode: bool,
-
-    fn init() Parser {
-        return .{
-            .current = undefined,
-            .previous = undefined,
-            .hadError = false,
-            .panicMode = false,
-        };
-    }
-
-    /// Prints where the error occurred.
-    /// Sets the error flag and going panic mode instead of immediately returning compile error,
-    /// because we want to resynchronize and keep on parsing.
-    /// Thus, after a first error is detected, any other errors will get suppressed.
-    /// Panic mode ends when the parser hits a synchronization point (i.e. statement boundaries).
-    fn errorAt(self: *Parser, token: *Token, message: []const u8) void {
-        if (self.panicMode) return;
-        self.panicMode = true;
-        std.debug.print("[line {d}] Error", .{token.line});
-
-        if (token.type == TokenType.EOF) {
-            std.debug.print(" at end", .{});
-        } else if (token.type == TokenType.ERROR) {
-            // Nothing
-        } else {
-            std.debug.print(" at '{s}'", .{token.lexeme});
-        }
-
-        std.debug.print(": {s}\n", .{message});
-        self.hadError = true;
-    }
-
-    fn @"error"(self: *Parser, message: []const u8) void {
-        self.errorAt(&self.previous, message);
-    }
-
-    fn errorAtCurrent(self: *Parser, message: []const u8) void {
-        self.errorAt(&self.current, message);
-    }
-};
