eeb5981239117be0b5c6d6b593ef1106f5f3c950
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-02-19 21:05:03 +0900

    Compiler front end barebones

--- src/Compiler.zig
+++ src/Compiler.zig
@@ -1,36 +1,103 @@
 const std = @import("std");
 const Chunk = @import("Chunk.zig");
 const Scanner = @import("Scanner.zig");
+const Token = Scanner.Token;
+const TokenType = Scanner.TokenType;
+const InterpretError = @import("VM.zig").InterpretError;
 
 /// Compiler struct.
 const Compiler = @This();
 
+const Parser = struct {
+    current: Token,
+    previous: Token,
+    hadError: bool,
+    panicMode: bool,
+
+    fn init() Parser {
+        return .{
+            .current = undefined,
+            .previous = undefined,
+            .hadError = false,
+            .panicMode = false,
+        };
+    }
+
+    /// Prints where the error occurred.
+    /// Sets the error flag and going panic mode instead of immediately returning compile error,
+    /// because we want to resynchronize and keep on parsing.
+    /// Thus, after a first error is detected, any other errors will get suppressed.
+    /// Panic mode ends when the parser hits a synchronization point (i.e. statement boundaries).
+    fn errorAt(self: *Parser, token: *Token, message: []const u8) void {
+        if (self.panicMode) return;
+        self.panicMode = true;
+        std.debug.print("[line {d}] Error", .{token.line});
+
+        if (token.type == TokenType.EOF) {
+            std.debug.print(" at end", .{});
+        } else if (token.type == TokenType.ERROR) {
+            // Nothing
+        } else {
+            std.debug.print(" at '{s}'", .{token.lexeme});
+        }
+
+        std.debug.print(": {s}\n", .{message});
+        self.hadError = true;
+    }
+
+    fn @"error"(self: *Parser, message: []const u8) void {
+        self.errorAt(&self.previous, message);
+    }
+
+    fn errorAtCurrent(self: *Parser, message: []const u8) void {
+        self.errorAt(&self.current, message);
+    }
+};
+
 source: []const u8,
 destination: *Chunk,
 scanner: Scanner,
+parser: Parser,
 
 pub fn init(source: []const u8, destination: *Chunk) Compiler {
     return .{
         .source = source,
         .destination = destination,
         .scanner = Scanner.init(source),
+        .parser = undefined,
     };
 }
 
-/// Compile the given source code.
+/// Compiles the given source code.
 pub fn run(self: *Compiler) !void {
-    // FIXME: currently this is temporary driver code
-    var line: isize = -1;
-    while (true) {
-        const token = self.scanner.scanToken();
-        if (token.line != line) {
-            std.debug.print("{d: >4} ", .{token.line});
-            line = token.line;
-        } else {
-            std.debug.print("{s: >4} ", .{"|"});
-        }
-        std.debug.print("{s: >13} '{s}'\n", .{ @tagName(token.type), token.lexeme });
+    // Front end
+    self.parser = Parser.init();
 
-        if (token.type == Scanner.TokenType.EOF) break;
+    self.advance();
+    // self.expression();
+    self.consume(TokenType.EOF, "Expect end of expression.");
+    return if (self.parser.hadError) InterpretError.INTERPRET_COMPILE_ERROR;
+}
+
+/// Steps forward through the token stream.
+/// It asks the scanner for the next token and stores it for later use.
+fn advance(self: *Compiler) void {
+    self.parser.previous = self.parser.current;
+
+    while (true) {
+        self.parser.current = self.scanner.scanToken();
+        if (self.parser.current.type != TokenType.ERROR) break;
+
+        self.parser.errorAtCurrent(self.parser.current.lexeme);
     }
+}
+
+/// Consumes next token whilst validating its type at the same time.
+fn consume(self: *Compiler, @"type": TokenType, message: []const u8) void {
+    if (self.parser.current.type == @"type") {
+        self.advance();
+        return;
+    }
+
+    self.parser.errorAtCurrent(message);
 }
