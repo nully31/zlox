940e6bf24e61313626cef76cb261329baa10e5ab
Masayuki Kawasaki <nu11p0er@gmail.com>
2024-12-09 21:04:46 +0900

    Add string comparison to `isEqual()` method

--- src/value.zig
+++ src/value.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const object = @import("object.zig");
 const Object = object.Object;
+const ObjString = object.ObjString;
 
 /// A constant's type that zlox handles.
 pub const ValueType = enum { boolean, nil, number, obj };
@@ -16,7 +17,13 @@ pub const Value = union(ValueType) {
     }
 
     pub fn isEqual(self: Value, b: Value) bool {
-        return std.meta.eql(self, b);
+        return switch (self) {
+            .obj => |a| blk: {
+                if (!b.is(ValueType.obj)) break :blk false;
+                break :blk std.mem.eql(u8, @as(*ObjString, @ptrCast(a)).chars, @as(*ObjString, @ptrCast(b.obj)).chars);
+            },
+            else => std.meta.eql(self, b),
+        };
     }
 
     pub fn print(self: Value) void {
@@ -50,4 +57,22 @@ test "compare values" {
     try std.testing.expect(!a.isEqual(b));
     a = Value{ .nil = {} };
     try std.testing.expect(a.isEqual(b));
+}
+
+test "compare strings" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var string = Object{ .string = ObjString{ .allocator = allocator, .init_chars = "test" } };
+    const string1 = try string.allocate();
+    const a = Value{ .obj = string1 };
+    var b = a;
+    try std.testing.expect(a.isEqual(b));
+
+    string = Object{ .string = ObjString{ .allocator = allocator, .init_chars = "test2" } };
+    const string2 = try string.allocate();
+    b = Value{ .obj = string2 };
+    try std.testing.expect(!a.isEqual(b));
+
+    b = Value{ .boolean = false };
+    try std.testing.expect(!a.isEqual(b));
 }
